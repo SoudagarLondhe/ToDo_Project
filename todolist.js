ff0790faae02626f30b11b19bf6863b207fd55a7
SoudagarLondhe <80147267+SoudagarLondhe@users.noreply.github.com>
2021-08-20 10:33:31 +0530

    added append list

---
+++ todolist.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('#input').change(function(){
+        var input=$(this).val();
+        $('ul').append('<li>' + input + '<i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
+        $(this).val('');
+    });
+
+});
